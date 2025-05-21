75a2b801a313c3f94b54af93d18e96ed6aa57044
Igor Pavlov <103071674+IgorPavlov00@users.noreply.github.com>
2025-05-21 13:24:51 +0200

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -51,20 +51,21 @@ const Navbar = () => {
       </ul>
 
       <div className="lg:flexCenter hidden">
-        <Button
-          type="button"
-          title="Contact us"
-          icon="/user.svg"
-          variant="btn_dark_green"
-          onClick={() => {
-            const footer = document.querySelector("footer");
-            if (footer) {
-              footer.scrollIntoView({ behavior: "smooth" });
-            } else {
-              console.error("Footer element not found!");
-            }
-          }}
-        />
+          <Button
+            type="button"
+            title="Contact us"
+            icon="/user.svg"
+            variant="btn_dark_green"
+            onClick={() => {
+              const footer = document.querySelector("footer");
+              if (footer) {
+                footer.scrollIntoView({ behavior: "smooth" });
+              } else {
+                console.error("Footer element not found!");
+              }
+            }}
+          />
+
       </div>
 
       <Image
