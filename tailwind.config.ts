50e7a1ab4ab01150b4621e262ccdd451985f3485
Igor Pavlov <103071674+IgorPavlov00@users.noreply.github.com>
2025-05-21 14:05:09 +0200

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -33,7 +33,7 @@ module.exports = {
         "bg-img-1": "url('/img-1.png')",
         "bg-img-2": "url('/img-2.png')",
         "feature-bg": "url('/')",
-        pattern: "url('/iphone.png')",
+        pattern: "url('/pattern.png')",
         "pattern-2": "url('/pattern-bg.png')",
       },
       screens: {
